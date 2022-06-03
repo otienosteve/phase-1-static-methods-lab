b78590de9728f53900775617954ea833381d0494
otienosteve <steveotieno701@gmail.com>
2022-06-03 16:41:32 +0300

    Completed assignement

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
-  //add static methods here
+    static capitalize(str) {
+        return str.charAt(0).toUpperCase() + str.split('').splice(1, str.length).join('')
+
+
+    }
+    static sanitize(str) {
+        var c = str.match(/[a-z0-9-\s']/gi)
+        if (c !== null) {
+            return c.join('')
+        } else {
+            return ''
+        }
+
+    }
+    static titleize(str) {
+        let arr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+        let strArr = str.split(' ')
+        let newArr = []
+        for (var f = 0; f <= strArr.length - 1; f++) {
+            if (f === 0) {
+                newArr.push(strArr[f].charAt(0).toUpperCase() + strArr[f].split('').splice(1, strArr[f].length).join(''))
+                continue
+            }
+            if (arr.includes(strArr[f])) {
+                newArr.push(strArr[f])
+            } else {
+                newArr.push(strArr[f].charAt(0).toUpperCase() + strArr[f].split('').splice(1, strArr[f].length).join(''))
+            }
+
+        }
+        return newArr.join(' ')
+    }
 }
